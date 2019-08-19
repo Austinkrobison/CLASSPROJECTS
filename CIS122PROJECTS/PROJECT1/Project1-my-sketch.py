a1c049c3b9a4243dbaf190606e7e134ac4ce8dfd
Austin Robison <austinkrobison@gmail.com>
2019-08-18 17:22:42 -0700

    fixed typo

--- CIS122PROJECTS/PROJECT1/Project1-my-sketch.py
+++ CIS122PROJECTS/PROJECT1/Project1-my-sketch.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # project1-my-sketch.py
 
 import turtle
@@ -36,42 +36,3 @@ def spiral(n, angle, step):
         turtle.forward(step)
         turtle.left(angle)
 spiral(100,150,140)
-=======
-# project1-my-sketch.py
-
-import turtle
-turtle.speed(100)
-
-turtle.pencolor('cyan')
-turtle.pensize(width=4)
-def spiral(n, angle, step):
-    for step in range(n):
-        turtle.forward(step)
-        turtle.left(angle)
-spiral(155,85,140)
-
-turtle.penup()
-turtle.forward(200)
-turtle.pendown()
-
-turtle.pencolor('purple')
-turtle.pensize(width=2)
-def spiral(n, angle, step):
-    for step in range(n):
-        turtle.forward(step)
-        turtle.left(angle)
-spiral(100,120,140)
-
-
-turtle.penup()
-turtle.forward(350)
-turtle.pendown()
-
-turtle.pencolor('green')
-turtle.pensize(width=3)
-def spiral(n, angle, step):
-    for step in range(n):
-        turtle.forward(step)
-        turtle.left(angle)
-spiral(100,150,140)
->>>>>>> c78d8bf239e181060e73dfeff3878afb65610151
