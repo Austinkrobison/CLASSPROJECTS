d3eb32b4b318ecec94b6ffe81e5d8dac2477063d
Austin Robison <austinkrobison@gmail.com>
2019-08-18 17:24:06 -0700

    fixed typos

--- CIS122PROJECTS/PROJECT1/project1-spiral.py
+++ CIS122PROJECTS/PROJECT1/project1-spiral.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # project1-spiral.py
 
 import turtle
@@ -8,14 +8,3 @@ def spiral(n, angle, step):
         turtle.forward(step)
         turtle.left(angle)
 spiral(120,60,10)
-=======
-# project1-spiral.py
-
-import turtle
-turtle.pencolor('cyan')
-def spiral(n, angle, step):
-    for step in range(n):
-        turtle.forward(step)
-        turtle.left(angle)
-spiral(120,60,10)
->>>>>>> c78d8bf239e181060e73dfeff3878afb65610151
