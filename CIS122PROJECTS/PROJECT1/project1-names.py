8405f6bef738814b1e5ee8f6f3b9db885b293df6
Austin Robison <austinkrobison@gmail.com>
2019-08-18 17:23:44 -0700

    fixed typos

--- CIS122PROJECTS/PROJECT1/project1-names.py
+++ CIS122PROJECTS/PROJECT1/project1-names.py
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
+
 # Project1-names.py
 
 name = ("Barbara")
 
 for i in range(7):
     print("Hello" + " " + name)
-
-=======
-# Project1-names.py
-
-name = ("Barbara")
-
-for i in range(7):
-    print("Hello" + " " + name)
-
->>>>>>> c78d8bf239e181060e73dfeff3878afb65610151
